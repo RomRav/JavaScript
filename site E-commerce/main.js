3295876a5ce7dccb793939f8076a98dc77105aa3
RomRav <romain.ravault@gmail.com>
2022-01-13 16:51:25 +0100

    Ajout de la ref, du prix dans la carte produit et gestion de l'ajout d'un produit dans le panier.

--- site E-commerce/main.js
+++ site E-commerce/main.js
@@ -3,64 +3,74 @@ let imgCard;
 let cardBody;
 let cardTitle;
 let cardText;
-let productId = 0;
+let refPriceText;
 let productsDiv = document.getElementById('product-list');
+let productInBasket;
+let basket = [];
 let selectedCategoryTab = [];
 let painChocolat = {
     name: 'Pain au chocolat',
     img: 'pains-au-chocolat.jpg',
     detail: 'Pâte levée feuilletée fourrée de deux barres de chocolat.',
     category: 'Viennoiseries',
-    prix: 1.50
+    price: 1.50,
+    ref: 1
 };
 let croissant = {
     name: 'Croissant',
     img: 'croissant.jpg',
     detail: 'Pâte levée feuilletée.',
     category: 'Viennoiseries',
-    prix: 1.30
+    price: 1.30,
+    ref: 2
 };
 let eclaire = {
     name: 'Eclaire au chocolat',
     img: 'eclaire.jpg',
     detail: 'Pâte à choux foirrée de crème au chocolat.',
     category: 'Patisseries',
-    prix: 3.10
+    price: 3.10,
+    ref: 3
 };
 let choux = {
     name: 'Choux à la crème',
     img: 'choux.jpg',
     detail: 'Pâte à choux foirrée de Crème pâtissière.',
     category: 'Patisseries',
-    prix: 2
+    price: 2,
+    ref: 4
 };
 let macaron = {
     name: 'Macaron',
     img: 'macarons.jpg',
     detail: 'Assortiment de macarons (Framboise, Café, Chocolat/Passion, Vanille).',
     category: 'Patisseries',
-    prix: 4
+    price: 4,
+    ref: 5
 };
 let campagne = {
     name: 'Pain de campagne',
     img: 'campagne.jpg',
     detail: 'Pain aux goûts et caractéristiques du pain ordinaire des campagnes d\'autrefois.',
     category: 'Pains',
-    prix: 4
+    price: 4,
+    ref: 6
 };
 let baguette = {
     name: 'Pain baguette',
     img: 'baguettes.jpg',
     detail: 'Pain croustillant de forme allongé.',
     category: 'Pains',
-    prix: 1.10
+    price: 1.10,
+    ref: 7
 };
 let boule = {
     name: 'Boule',
     img: 'boule.jpg',
     detail: 'Pain rustique en forme de boule.',
     category: 'Pains',
-    prix: 1.50
+    price: 1.50,
+    ref: 8
 };
 let products = [baguette, campagne, macaron, choux, eclaire, croissant, painChocolat, boule];
 //Ecoute du click
@@ -75,56 +85,63 @@ document.addEventListener('click', event =>{
     if(event.target.matches('.Patisseries')){
         selectedCategoryManager('Patisseries');
     }
-    if(event.target.matches('.Panier')){}   
+    if(event.target.matches('.Panier')){}  
+    if(event.target.matches('.addProductBtn')){
+        toBasket(findProduct(event.target.id));
+    } 
 });
+//Ajout le produit au tableau panier
+function toBasket(product){
+    productInBasket = new Object();
+    productInBasket.product = product;
+    productInBasket.quantity = 1;
+    basket.push(productInBasket);
+    console.log(basket)
+}
+//Trouve un produit dans la liste des produits
+function findProduct(id){
+    for (const product of products) {
+        if(product.ref == id)
+            return product;
+    }
+}
 //Boucle sur les produits et les affiches en fonction de la catégorie choisit.
 function selectedCategoryManager(category){
     productsDiv.innerHTML="";
     for (const product of products) {
-        console.log('cat : ' + category)
-        console.log(product.category)
         if(product.category == category){
-            console.log(product.category)
             cardMaker(product);
         }
     }
-
-    /* switch(category){
-        case 'Pains' :
-            selectedCategoryTab;
-            break;
-        case 'Patisseries' :
-
-            selectedCategoryTab;
-            break;
-        case 'Viennoiseries' :
-            selectedCategoryTab;
-            break;
-        default:
-            break;
-    }
-    */
 }
 //Crée une carte de produit.
 function cardMaker(product){
     //Création des élèments de la carte
+    //carte
     card = document.createElement('div');
     card.className = 'card m-auto';
     card.style = 'width: 18rem';
+    //corp de la carte
     cardBody = document.createElement('div');
     cardBody.className = 'card-body';
+    //image de la carte
     imgCard = imageMaker(`./images/${product.img}`, 'card-img-top');
+    //Titre de la carte
     cardTitle = titleMaker(product.name, 'card-title');
+    //Texte de la carte
     cardText = paragraphMaker(product.detail, 'card-text');
-    cardBtn = buttonMaker(productId ,'btn btn-primary');
+    //Référence et prix
+    refPriceText = paragraphMaker(`ref : ${product.ref} / Prix : ${product.price}€`, 'card-text')
+    //Bouton de la carte
+    cardBtn = buttonMaker(product.ref ,'btn btn-primary addProductBtn');
     //Ajout des élémént dans la carte
     card.appendChild(imgCard);
     cardBody.appendChild(cardTitle);
     cardBody.appendChild(cardText);
+    cardBody.appendChild(refPriceText);
     cardBody.appendChild(cardBtn);
     card.appendChild(cardBody);
     productsDiv.appendChild(card);
-    productId++;
     return card;
 }
 //Création de l'image de la carte
